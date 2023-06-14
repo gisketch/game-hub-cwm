032ab81c8a1c07469e644a27cd5d50d5a04991b4
gisketch <agjmnz@gmail.com>
2023-06-14 16:29:46 +0800

    added highlight for selected genre in genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -14,9 +14,10 @@ import GenreListSkeleton from './GenreListSkeleton'
 
 interface Props {
   onSelectGenre: (genre: Genre) => void
+  selectedGenre: Genre | null
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres()
   const skeletonMap = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
@@ -40,6 +41,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               onClick={() => onSelectGenre(genre)}
               variant="link"
               fontSize="lg"
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
             >
               {genre.name}
             </Button>
