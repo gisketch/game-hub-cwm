a9cfa507547c866bf9edf26f942a6a14cec7023e
gisketch <agjmnz@gmail.com>
2023-06-19 20:53:42 +0800

    refactor- simplified time calcs using ms package

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
-import APIClient, { FetchResponse } from '../services/apiClient'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import ms from 'ms'
 import { GameQuery } from '../App'
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
+import APIClient, { FetchResponse } from '../services/apiClient'
 import { Platform } from './usePlatforms'
 
 const apiClient = new APIClient<Game>('/games')
@@ -30,7 +31,7 @@ const useGames = (gameQuery: GameQuery) => {
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined
     },
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms('24h'),
   })
 }
 
