71508a149654d70fc9582fd9feb100af74c8b449
gisketch <agjmnz@gmail.com>
2023-06-21 22:34:13 +0800

    Fixed NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
 import useGameQueryStore from '../store'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <NavLink to="">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </NavLink>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,19 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { useRef } from 'react'
 import { BsSearch } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 import useGameQueryStore from '../store'
 
 const SearchInput = () => {
-  const setSearchText = useGameQueryStore((s) => s.setSearchText)
-
   const ref = useRef<HTMLInputElement>(null)
+  const setSearchText = useGameQueryStore((s) => s.setSearchText)
+  const navigate = useNavigate()
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault()
+        navigate('/')
         if (ref.current) setSearchText(ref.current.value)
       }}
     >
