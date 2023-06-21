1fdc72233e80653695528df8b0fa7965cad5f4f8
gisketch <agjmnz@gmail.com>
2023-06-21 22:25:43 +0800

    Built game screenshots

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -4,9 +4,11 @@ import { Trailer } from '../entities/Trailer'
 
 const useTrailers = (gameId: number) => {
   const apiClient = new APIClient<Trailer>(`games/${gameId}/movies`)
+
   return useQuery({
     queryKey: ['trailers', gameId],
     queryFn: apiClient.getAll,
   })
 }
+
 export default useTrailers
