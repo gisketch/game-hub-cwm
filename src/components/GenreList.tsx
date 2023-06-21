95a09f11804068dc1a1f3c14e4402dcbe8abaab8
gisketch <agjmnz@gmail.com>
2023-06-21 22:39:12 +0800

    final commit

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,7 +10,7 @@ import {
   Text,
 } from '@chakra-ui/react'
 import useGenres from '../hooks/useGenres'
-import { Genre } from '../entities/Genre'
+import Genre from '../entities/Genre'
 import getCroppedImageUrl from '../services/image-url'
 import GenreListSkeleton from './GenreListSkeleton'
 import useGameQueryStore from '../store'
